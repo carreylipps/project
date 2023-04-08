f3a8fda6f8a0563c8bd68b194ba485f6fbecf509
carreylipps <85637986+carreylipps@users.noreply.github.com>
2023-04-07 21:32:58 -0500

    Create tickets.js

---
+++ tickets.js
@@ -0,0 +1,17 @@
+const tickets = [
+  {
+  "id": 1,    
+  "created_at": "2023-04-07T20:34:14Z",   
+  "updated_at": "2023-04-07T21:12:56Z",    
+  "type": "ticket",    
+  "subject": "project ",    
+  "description": "Phase 1 text",   
+  "priority": "high",    
+  "status": "open",    
+  "recipient": "you",    
+  "submitter": "Carrey Lipps",   
+  "assignee_id": 34,    
+  "follower_ids": [12345, 455667],
+  "tags": ["winner", "tickets"]
+}
+  ]
