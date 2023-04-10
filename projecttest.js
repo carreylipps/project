53f61e0b4e8abeadfd2b10cfe00811a9d9a598af
carreylipps <85637986+carreylipps@users.noreply.github.com>
2023-04-10 16:03:15 -0500

    Update projecttest.js

--- projecttest.js
+++ projecttest.js
@@ -1,8 +1,7 @@
-const { MongoClient, ObjectId } = require("mongodb");  // Added ObjectId to import
+const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
 const uri = "mongodb+srv://carrey:beauxbella@clmdb.xd8c4zo.mongodb.net/?retryWrites=true&w=majority";
-
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -15,89 +14,67 @@ console.log('Server started at http://localhost:' + port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Default route:
-app.get('/', function(req, res) {
-  const myquery = req.query;
-  var outstring = 'Starting... ';
-  res.send(outstring);
-});
-
-app.get('/say/:name', function(req, res) {
-  res.send('Hello ' + req.params.name + '!');
-});
-
-
-// Route to access database by ticket ID:
-app.get('/rest/ticket/:id', function(req, res) {  // Changed route to include ':id' parameter
+// Route to access database:
+app.get('/rest/ticket/:id', async function(req, res) {
   const client = new MongoClient(uri);
+  const searchKey = { ticketID: req.params.id };
+  console.log("Looking for: " + searchKey);
 
-  async function run() {
-    try {
-      await client.connect();  // Added connect() method
-      const database = client.db('clmdb');
-      const tickets = database.collection('cmps415');
+  try {
+    await client.connect();
+    const database = client.db('clmdb');
+    const tickets = database.collection('cmps415');
 
-      const ticket = await tickets.findOne({ _id: ObjectId(req.params.id) });  // Changed query to use _id and ObjectId
-      console.log(ticket);
-      res.send('Found this: ' + JSON.stringify(ticket));  //Use stringify to print a json
-
-    } catch (err) {  // Added error handling
-      console.log(err);
-      res.send(err.message).status(500);
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
+    const ticket = await tickets.findOne(searchKey);
+    console.log(ticket);
+    res.send('Found this: ' + JSON.stringify(ticket));  // Use stringify to print a JSON
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  } finally {
+    await client.close();
   }
-
-  run().catch(console.dir);
 });
 
-// Route to add a new ticket to the database:
-app.post("/rest/ticket/", function(req,res){
+app.post("/rest/ticket/", async function(req, res) {
   console.log("Adding ticket ");
 
   const client = new MongoClient(uri);
 
-  async function run() {
-    try{
-      await client.connect();  // Added connect() method
-      const database = client.db("clmdb");
-      const tickets = database.collection("cmps415");  // Changed collection name to 'cmps415'
-
-      let newDocument = req.body;
-      newDocument._id = new ObjectId();  // Added _id using ObjectId
-      let result = await tickets.insertOne(newDocument);
-      res.send(result).status(204);
-    } catch (err) {  // Added error handling
-      console.log(err);
-      res.send(err.message).status(500);
-    } finally{
-      await client.close();
-    }
+  try {
+    await client.connect();
+    const database = client.db("clmdb");
+    const ticket = database.collection("ticket");
+    let newDocument = req.body;
+    newDocument.id = "574628";
+    let result = await ticket.insertOne(newDocument);
+    res.status(204).send();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  } finally {
+    await client.close();
   }
-  run().catch(console.dir);
 });
 
-// Route to get a list of all tickets:
-app.get('/rest/list/', function(req, res) {
-  console.log("Looking for every ticket");
+app.get('/rest/list/', async function(req, res) {
+  console.log("looking for every ticket");
 
   const client = new MongoClient(uri);
 
-  async function run() {
-    try {
-      await client.connect();  // Added connect() method
-      const database = client.db('clmdb');
-      const tickets = database.collection('cmps415');
+  try {
+    await client.connect();
+    const database = client.db('clmdb');
+    const ticket = database.collection('Ticket');
 
-      let results = await tickets.find({})
-        .toArray();
+    let results = await ticket.find({})
+      .toArray();
 
-      res.send(results).status(200);
-    } catch (err) {  // Added error handling
-      console.log(err);
-      res.send(err.message).status(500);
-    } finally {
-      await client.close;
-    }
+    res.send(results).status(200);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  } finally {
+    await client.close();
+  }
+});
